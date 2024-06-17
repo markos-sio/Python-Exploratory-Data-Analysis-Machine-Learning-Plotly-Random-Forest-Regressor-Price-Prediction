0ad5ce74baf82e163f81300d36e612a76c6a6c5b
Markos Siozos <markossio@hotmail.com>
2024-06-17 12:58:16 +0300

    Use kfold cross validattion instead of simple split

--- flight_price_prediction.py
+++ flight_price_prediction.py
@@ -6,11 +6,11 @@ Created on Sun Jun 16 16:41:54 2024
 """
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error,r2_score
-import math
 import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold, cross_val_predict
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import math
 
 
 # Importing the csv file and creating a dataframe using pandas
@@ -91,40 +91,36 @@ print(data.head())
 data_without_price = data.drop("price", axis=1) # Excluding the target variable 'price' column
 X, y = data_without_price, data["price"] # Features are everything except 'price', target is 'price'
 
-# Spliting the data into training and testing sets, with 20% of the data for testing and 80% for training
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) #random_state for ensuring that the random process produce the same results in every run
-
 # Initialize the RandomForestRegressor model
 rf = RandomForestRegressor()
 
-# Train the model on the training data
-rf.fit(X_train, y_train)
+# Using K-Fold Cross Validation
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
-# Calculate and print the R^2 score on the test data
-score = rf.score(X_test, y_test)
-print(score)
-
-# Predict the prices on the test data
-y_pred = rf.predict(X_test)
+# Cross-validation predictions
+y_pred = cross_val_predict(rf, X, y, cv=kf)
 
 # Calculate and print the R^2 score
-print('R Squared', r2_score(y_test, y_pred))
+print('R Squared', r2_score(y, y_pred))
 
 # Calculate and print the Mean Absolute Error
-print('Mean Absolute Error', mean_absolute_error(y_test, y_pred))
+print('Mean Absolute Error', mean_absolute_error(y, y_pred))
 
 # Calculate and print the Mean Squared Error
-print('Mean Squared Error', mean_squared_error(y_test, y_pred))
+print('Mean Squared Error', mean_squared_error(y, y_pred))
 
 # Calculate and print the Root Mean Squared Error
-print('Root Mean Squared', math.sqrt(mean_squared_error(y_test, y_pred)))
+print('Root Mean Squared', math.sqrt(mean_squared_error(y, y_pred)))
 
 # Plot the predicted prices vs the original prices
-plt.scatter(y_test, y_pred)
+plt.scatter(y, y_pred)
 plt.xlabel("Original Price")
 plt.ylabel("Predicted Prices")
 plt.title("Predicted Prices vs Original Prices")
 plt.show()
+
+# Train the model on the entire dataset to get feature importances
+rf.fit(X, y)
 
 # Calculate and print feature importances from the trained RandomForest model
 importances = dict(zip(rf.feature_names_in_, rf.feature_importances_))
